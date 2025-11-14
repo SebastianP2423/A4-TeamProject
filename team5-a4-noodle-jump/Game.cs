a75acddba7dbf0d88ca4a9c9e50a19398014fd06
Daniel <daniel.nguyen8@mohawkcollege.ca>
2025-11-14 10:58:42 -0500

    Create Visual Studio Project + foundational stuff

---
+++ team5-a4-noodle-jump/Game.cs
@@ -0,0 +1,37 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+using team5_a4_noodle_jump;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        // Place your variables here:
+        Player player = new Player();
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+            Window.SetSize(400, 600);
+            Window.SetTitle("Noodle Jump!");
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+            Window.ClearBackground(Color.OffWhite);
+            player.Update();
+
+        }
+    }
+
+}
---
+++ team5-a4-noodle-jump/Player.cs
@@ -0,0 +1,20 @@
+﻿using MohawkGame2D;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace team5_a4_noodle_jump
+{
+    internal class Player
+    {
+        Vector2 position = new Vector2(200, 400);
+        public void Update()
+        {
+            position.X = Input.GetMouseX();
+            Draw.Square(position, 30);
+        }
+    }
+}
