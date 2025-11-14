59f14be6cd1bc63a387a576593ef4ef438064db0
Daniel <daniel.nguyen8@mohawkcollege.ca>
2025-11-14 11:31:14 -0500

    Add gravity to player class
    For now it's very very very rudimental, just enough to make sure it kinda works and I'll know what to do when it comes time to add platforms.

--- team5-a4-noodle-jump/Player.cs
+++ team5-a4-noodle-jump/Player.cs
@@ -10,11 +10,29 @@ namespace team5_a4_noodle_jump
 {
     internal class Player
     {
+        int squareSize = 30;
         Vector2 position = new Vector2(200, 400);
+        float yVelocity = 0f;
+        float gravity = 10f;
+        float gravityForce = 0f;
         public void Update()
         {
-            position.X = Input.GetMouseX();
-            Draw.Square(position, 30);
+            position.X = Input.GetMouseX() - squareSize / 2;
+
+            if (position.Y + squareSize * 1.1f <= 600)
+            {
+                
+            }
+            gravityForce = gravity * Time.DeltaTime;
+            yVelocity += gravityForce;
+            position.Y += yVelocity;
+
+            Draw.Square(position, squareSize);
+
+            if (Input.IsKeyboardKeyPressed(KeyboardInput.Space))
+            {
+                yVelocity *= -1; yVelocity -= 2;
+            }
         }
     }
 }
