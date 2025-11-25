782b75023890e48be5e2074d78f96bb53bb7b349
SebastianP2423 <000946282@mohawkcollege.ca>
2025-11-24 19:47:57 -0500

    Platforms have movement now and can now scroll upwards to continue the climb like they do in the original game. Scrolling and movement implemented

--- team5-a4-noodle-jump/GAME 10003 Game Development Foundations - 2D Game Template (v1.4)1/Platform.cs
+++ team5-a4-noodle-jump/GAME 10003 Game Development Foundations - 2D Game Template (v1.4)1/Platform.cs
@@ -1,19 +1,32 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Numerics;
 
-public class Platform;
 
 namespace MohawkGame2D
-{
-        public class Platform // Platfroms do not scroll like the game yet
+    {
+        public class Platform
         {
             public Vector2 Position;
             public Vector2 Size;
             public Vector2 Velocity = Vector2.Zero;
 
+            // Actual moving platfform functions 
+            public bool IsMoving = false;
+            public float MoveSpeed = 50f;       // how fast thy move
+            public float MoveDistance = 50f;    // how far their movement is
+
+            private float moveTimer = 0f;
+            private Vector2 startPos;
+
+            // Scrolling for platforms like the actual game
+            public float ScrollSpeed = -100f; 
+
             public Platform(float x, float y, float width, float height)
             {
                 Position = new Vector2(x, y);
                 Size = new Vector2(width, height);
+
+                startPos = Position; 
             }
 
             public float Left => Position.X;
@@ -23,15 +36,26 @@ namespace MohawkGame2D
 
             public void Update(float deltaTime)
             {
+                //Horizontal movement
+                if (IsMoving)
+                {
+                    moveTimer += deltaTime * MoveSpeed;
+                    float xOffset = (float)Math.Sin(moveTimer) * MoveDistance;
+                    Position = new Vector2(startPos.X + xOffset, Position.Y);
+                }
+
+                // Scroll upward feature
+                Position += new Vector2(0, ScrollSpeed * deltaTime);
+
+                // Just in case something else needs to set velocity
                 Position += Velocity * deltaTime;
             }
 
             public void Draw()
             {
-                // colours for the platforms whch only need to be called 
-                Draw.FillColor = Color.Red;
-                Draw.LineColor = Color.Red;
-                Draw.LineSize  = 2;
+                Draw.FillColor = IsMoving ? Color.Green : Color.Red;
+                Draw.LineColor = Draw.FillColor;
+                Draw.LineSize = 2;
 
                 Draw.Rectangle(Position, Size);
             }
@@ -42,12 +66,8 @@ namespace MohawkGame2D
                 float pRight = playerPos.X + playerSize.X;
                 float pBottom = playerPos.Y + playerSize.Y;
 
-                bool horizontallyAligned =
-                    pRight > Left &&
-                    pLeft < Right;
-
+                bool horizontallyAligned = pRight > Left && pLeft < Right;
                 bool movingDown = playerVelocity.Y > 0;
-
                 bool crossingTop =
                     pBottom <= Top &&
                     pBottom + playerVelocity.Y >= Top;
@@ -56,6 +76,8 @@ namespace MohawkGame2D
             }
         }
     }
+
+
 
 
 
