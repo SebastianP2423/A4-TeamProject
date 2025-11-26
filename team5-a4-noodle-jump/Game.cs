bfb7af34472a5fe3d02fa86a23d3910c8f321f1e
Riley-mckenna555 <000975214@mohawkcollege.ca>
2025-11-25 21:53:52 -0500

    add background picture

--- team5-a4-noodle-jump/Game.cs
+++ team5-a4-noodle-jump/Game.cs
@@ -13,7 +13,7 @@ namespace MohawkGame2D
     {
         // Place your variables here:
         Player player = new Player();
-
+        Texture2D background;
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
@@ -21,6 +21,7 @@ namespace MohawkGame2D
         {
             Window.SetSize(400, 600);
             Window.SetTitle("Noodle Jump!");
+            background = Graphics.LoadTexture("../../../assets/graphics/backgroundnoodle.png");
         }
 
         /// <summary>
@@ -29,8 +30,9 @@ namespace MohawkGame2D
         public void Update()
         {
             Window.ClearBackground(Color.OffWhite);
+            Graphics.Draw(background, 0, 0);
             player.Update();
-
+            
         }
     }
 
